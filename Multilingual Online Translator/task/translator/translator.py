d315e54326acb38ea515397e3b8d8d83a840d0fc
Anatol Derbisz <anatholius@gmail.com>
2022-06-28 12:19:19 +0200

    Initial commit

---
+++ Multilingual Online Translator/task/translator/translator.py
@@ -0,0 +1,6 @@
+lang = input(
+    'Type "en" if you want to translate from French into English, or "fr" if '
+    'you want to translate from English into French:'
+)
+word = input('Type the word you want to translate:')
+print(f'You chose "{lang}" as a language to translate "{word}".')
