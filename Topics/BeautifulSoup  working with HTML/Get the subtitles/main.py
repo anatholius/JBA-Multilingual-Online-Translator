49e0b89a3d354772ac83a10c59675f7f922c99be
Anatol Derbisz <anatholius@gmail.com>
2022-06-28 22:53:51 +0200

    `Python libraries`/`Networking`::`BeautifulSoup: working with HTML`: Get the subtitles ⌛✅

--- Topics/BeautifulSoup  working with HTML/Get the subtitles/main.py
+++ Topics/BeautifulSoup  working with HTML/Get the subtitles/main.py
@@ -1,3 +1,13 @@
 import requests
 
 from bs4 import BeautifulSoup
+
+
+def find_subtitle(index: int, url: str):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    res = soup.find_all('h2')[index]
+    print(res.text)
+
+
+find_subtitle(int(input()), input())
