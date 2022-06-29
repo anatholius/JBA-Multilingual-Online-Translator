d25410126919d020b439a4d2de098372bf640cf3
Anatol Derbisz <anatholius@gmail.com>
2022-06-29 11:00:13 +0200

    `Work on project`/`Stage 5/7`: Simultaneous translation 🖌✅ -> refactoring #3

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -8,34 +8,23 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+# speed up download
 s = requests.Session()
 
 
 class Translator:
-    LANGS = [
-        'arabic',
-        'german',
-        'english',
-        'spanish',
-        'french',
-        'hebrew',
-        'japanese',
-        'dutch',
-        'polish',
-        'portuguese',
-        'romanian',
-        'russian',
-        'turkish',
-    ]
+    LANGS = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew',
+             'japanese', 'dutch', 'polish', 'portuguese', 'romanian',
+             'russian', 'turkish']
     ADDRESS = 'https://context.reverso.net/translation'
 
     source_lang: int = None
     target_lang: int = None
     word: str
-    dinner_recipe: list = []
+    translation_board: list = []
     translations: list
     examples: zip
-    the_dinner = {}
+    translations_board = {}
 
     def __init__(self):
         print(*[
@@ -54,68 +43,56 @@ class Translator:
         self.word = word
 
         if self.target_lang < 0:
-            for lang, ingredients in enumerate(self.LANGS):
-                if lang == self.source_lang:
+            for t_lang_id, t_lang in enumerate(self.LANGS):
+                if t_lang_id == self.source_lang:
                     continue
-                self.cook_the_dish(ingredients, word)
-
-            # Remember the recipe for dinner
-            # print('Remember the recipe for dinner', file=sys.stderr)
+                self.translate_online(t_lang, word)
         else:
-            ingredients = self.LANGS[self.target_lang]
-            self.cook_the_dish(ingredients, word)
-            print('')
-            print(
-                'REMEMBER, you need to save file for specified target lag '
-                'too!!!',
-                file=sys.stderr)
-            print('')
+            target_language = self.LANGS[self.target_lang]
+            self.translate_online(target_language, word)
 
-        self.remember_recipe()
-        # Bring your multi-dish meal to the table, and say: dinner!
-        # print('dinner!', file=sys.stderr)
-        self.dinner()
+        # store translations to file
+        self.store_translation()
+
+        # print.report
+        self.report()
 
     def prepare_recipe(self):
-        dishes_count = len(self.the_dinner.keys())
-        self.dinner_recipe = []
+        dishes_count = len(self.translations_board.keys())
+        self.translation_board = []
         i = 0
-        for target, dish in self.the_dinner.items():
+        for target, dish in self.translations_board.items():
             if target == self.LANGS[self.source_lang]:
                 continue
             target_language = target.capitalize()
             nl = "" if i == 0 else "\n"
-            self.dinner_recipe.append(f'\n{nl}{target_language} Translations:')
+            self.translation_board.append(
+                f'\n{nl}{target_language} Translations:')
             t = dish['translations']
             translations = [t[0]] if dishes_count > 1 else t
 
             for t in translations:
-                self.dinner_recipe.append(f'{t}')
+                self.translation_board.append(f'{t}')
 
-            self.dinner_recipe.append(f'\n{target_language} Examples:')
+            self.translation_board.append(f'\n{target_language} Examples:')
 
             e = dish['examples']
             dish_examples = [list(e)[0]] if dishes_count > 1 else e
             examples = ['\n'.join(e) for e in dish_examples]
 
             for t in examples:
-                self.dinner_recipe.append(f'{t}')
+                self.translation_board.append(f'{t}')
             i = i + 1
 
-    def remember_recipe(self):
-        # preparing file
+    def store_translation(self):
         file_name = f'{self.word}.txt'
-        # file_exists = os.path.exists(file_name)
 
         with open(file_name, 'w') as file:
-            lines = [f'{r}\n' for recipe in self.dinner_recipe for r in recipe]
+            lines = [f'{r}\n' for rcp in self.translation_board for r in rcp]
             file.writelines(lines)
-            print(f'Saved in file: "{file_name}"', self.dinner_recipe)
+            print(f'File "{file_name}" saved.', file=sys.stderr)
 
-        # if file_exists:
-        #     print(f'File "{file_name}" was overridden.', file=sys.stderr)
-
-    def cook_the_dish(self, target: str, word: str):
+    def translate_online(self, target: str, word: str):
         target_lang = target.capitalize()
         url = f'{self.ADDRESS}/{self.direction(target)}/{word}'
         print(f'Request for {target_lang} translation to: {url}')
@@ -124,16 +101,19 @@ class Translator:
         print(f'{r.status_code} {r.reason}')
 
         if r.ok:
-            # Cook the pizza, ee.. soup
+            """Prepare translation board"""
+
+            # parse response
             soup = BeautifulSoup(r.content, 'html.parser')
+            # parse translations
             self.translations = [
                 t.text for i, t in
                 enumerate(soup.find_all('span', {'class': 'display-term'}))
             ]
-            # Cook examples translation soups
+            # parse examples
             source_soup = soup.find_all('div', {'class': 'src'})
             target_soup = soup.find_all('div', {'class': 'trg'})
-            # Prepare examples translation pairs with stripping values texts
+            # zip (pair) examples for use later
             self.examples = zip(
                 [e.text.strip() for e in source_soup if e.text.strip()],
                 [e.text.strip() for e in target_soup if e.text.strip()]
@@ -145,38 +125,47 @@ class Translator:
                 'examples': self.examples,
             }
 
-            def prepare_dish_recipe(source_lang, dishes_count):
+            def prepare_lang_translation(source_lang, translations_count):
                 if target == source_lang:
+                    # skip translation to source_lang
                     return None
+
                 recipe = []
                 target_language = target.capitalize()
 
                 recipe.append(f'{target_language} Translations:')
                 t = dish['translations']
-                translations = [t[0]] if dishes_count > 1 else t
+                translations = [t[0]] if translations_count > 1 else t
                 recipe += translations
 
                 recipe.extend(['', f'{target_language} Examples:'])
 
                 e = dish['examples']
-                examples = [list(e)[0]] if dishes_count > 1 else list(e)
-                recipe += [rcp for tr in examples for rcp in list(tr) + ['']]
+                # get all examples or just first if target_lang was `0`
+                ex = [list(e)[0]] if translations_count > 1 else list(e)
+                # flatten the array with examples pairs
+                recipe.extend([rcp for tr in ex for rcp in list(tr) + ['']])
+                # add new line between different languages translations
+                recipe.extend([''])
 
                 return recipe
 
-            dish_recipe = prepare_dish_recipe(
+            single_translation = prepare_lang_translation(
                 self.LANGS[self.source_lang],
-                len(self.the_dinner.keys())
+                len(self.translations_board.keys())
             )
-            if dish_recipe:
-                self.dinner_recipe.append(dish_recipe)
+            if single_translation:
+                self.translation_board.append(single_translation)
 
-            self.the_dinner[target] = dish
+            self.translations_board[target] = dish
 
-    def dinner(self):
-        dishes_count = len(self.the_dinner.keys())
-        print(f'There is {dishes_count}-dish dinner!', file=sys.stderr)
-        for line in self.dinner_recipe:
+    def report(self):
+        dishes_count = len(self.translations_board.keys())
+        print(
+            f'There is translations board with {dishes_count} languages!',
+            file=sys.stderr
+        )
+        for line in self.translation_board:
             print(*line, sep='\n')
 
 
@@ -186,8 +175,8 @@ Pass `-t` to the command that starts the program for local tests
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--test', action='store_true')
 if parser.parse_args().test:
-    # Translator().translate(3, 4, 'hello')
-    # Translator().translate(3, 0, 'hello')
+    Translator().translate(3, 4, 'hello')
+    Translator().translate(3, 0, 'hello')
     Translator().translate(12, 3, 'глаза')
     exit('Enjoy your meal!')
 
