67c3f33235117d1ae0549a70fed0480923ef4bc4
Anatol Derbisz <anatholius@gmail.com>
2022-06-29 04:38:43 +0200

    `Work on project`/`Stage 4/7`: All of them ✅ -> All tests passes

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,5 +1,5 @@
 """
-Local translator.
+Local translator with remote translating.
 """
 
 import argparse
@@ -7,51 +7,68 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 
-langs = {
-    'fr': 'french',
-    'en': 'english',
-}
-dirs = {
-    'fr': f'{langs["en"]}-{langs["fr"]}',
-    'en': f'{langs["fr"]}-{langs["en"]}',
-}
 
+class Translator:
+    LANGS = [
+        'arabic',
+        'german',
+        'english',
+        'spanish',
+        'french',
+        'hebrew',
+        'japanese',
+        'dutch',
+        'polish',
+        'portuguese',
+        'romanian',
+        'russian',
+        'turkish',
+    ]
+    source_lang: int = None
+    target_lang: int = None
 
-def translate(target_lang: str, word: str):
-    """
-    There are lies!
-    There in the description of the stage.
+    def __init__(self):
+        print(*[
+            'Hello, welcome to the translator. Translator supports:',
+            *[f'{i + 1}. {L.capitalize()}' for i, L in enumerate(self.LANGS)]
+        ], sep='\n')
 
-    Limiting the examples to 5 spoils tests!
-    """
+    def direction(self):
+        return f'{self.LANGS[self.source_lang]}-{self.LANGS[self.target_lang]}'
 
-    print(f'You chose "{target_lang}" as a language to translate "{word}".')
+    def translate(self, source: int, target: int, word: str):
+        self.source_lang = source - 1
+        self.target_lang = target - 1
 
-    url = f'https://context.reverso.net/translation/{dirs[target_lang]}/{word}'
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    print(f'{r.status_code} {r.reason}')
+        address = 'https://context.reverso.net/translation'
+        url = f'{address}/{self.direction()}/{word}'
+        print(f'Asking for translation to: {url}')
+        r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+        print(f'{r.status_code} {r.reason}')
 
-    if r.ok:
-        # cook the pizza, ee.. soup
-        soup = BeautifulSoup(r.content, 'html.parser')
-        translations = [t.text for i, t in enumerate(soup.find_all('span', {
-            'class': 'display-term'
-        }))]
+        if r.ok:
+            # Cook the pizza, ee.. soup
+            soup = BeautifulSoup(r.content, 'html.parser')
+            translations = [
+                t.text for i, t in
+                enumerate(soup.find_all('span', {'class': 'display-term'}))
+            ]
 
-        # cook examples translation soups
-        source_soup = soup.find_all('div', {'class': 'src ltr'})
-        target_soup = soup.find_all('div', {'class': 'trg ltr'})
-        # prepare examples translation pairs with stripping values texts
-        examples = zip(
-            [e.text.strip() for e in source_soup if e.text.strip()],
-            [e.text.strip() for e in target_soup if e.text.strip()]
-        )
+            # Cook examples translation soups
+            source_soup = soup.find_all('div', {'class': 'src ltr'})
+            target_soup = soup.find_all('div', {'class': 'trg ltr'})
+            # Prepare examples translation pairs with stripping values texts
+            examples = zip(
+                [e.text.strip() for e in source_soup if e.text.strip()],
+                [e.text.strip() for e in target_soup if e.text.strip()]
+            )
 
-        # Bring your meal to the table, dinner!
-        print(f'\n{langs[target_lang].capitalize()} Translations:')
-        print(*translations, sep='\n')
-        print(f'\n{langs[target_lang].capitalize()} Examples:')
-        print(*['\n'.join(e) for e in examples], sep='\n\n')
+            # Bring your meal to the table, dinner!
+            print(
+                f'\n{self.LANGS[self.target_lang].capitalize()} Translations:')
+            print(*translations, sep='\n')
+            print(f'\n{self.LANGS[self.target_lang].capitalize()} Examples:')
+            print(*['\n'.join(e) for e in examples], sep='\n\n')
 
 
 """
@@ -60,12 +77,11 @@ Pass -t for local tests to run program
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--test', action='store_true')
 if parser.parse_args().test:
-    translate('fr', 'hello')
+    Translator().translate(3, 4, 'hello')
     exit('Thanks for testing!')
 
-lang = input(
-    'Type "en" if you want to translate from French into English, or "fr" if '
-    'you want to translate from English into French:\n'
+Translator().translate(
+    int(input('Type the number of your language:\n')),
+    int(input('Type the number of language you want to translate to:\n')),
+    input('Type the word you want to translate:\n')
 )
-word = input('Type the word you want to translate:\n')
-translate(lang, word)
