a94c4c3071b0f549d8e794822c9f1d5ed4bcf8c8
Anatol Derbisz <anatholius@gmail.com>
2022-06-30 23:27:27 +0200

    `Additional learning`/`Tuples`::`Operations with tuple`: Unpacking ✅ -> First solution

--- Topics/Operations with tuple/Unpacking/main.py
+++ Topics/Operations with tuple/Unpacking/main.py
@@ -1,3 +1,22 @@
 def unpack(input_tuple):
-    # your code here
-    return unpacked
+    return tuple(
+        # first `for` to unpack comprehension
+        element for sublist in
+        (
+            # if `sub` is tuple, use it or use list(sub) -> [[], (), []]
+            [isinstance(sub, tuple) and sub or [sub] for sub in input_tuple]
+        )
+        # second `for` to unpack sub-list/sub-tuple
+        for element in sublist
+    )
+
+    # or easier way but with more ;) lines
+
+    # u = []
+    # for item in input_tuple:
+    #     if isinstance(item, tuple):
+    #         for i in item:
+    #             u.append(i)
+    #     else:
+    #         u.append(item)
+    # return tuple(u)
