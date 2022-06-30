b878c207946f018d434777031c1fbd3020617d03
Anatol Derbisz <anatholius@gmail.com>
2022-06-30 07:40:41 +0200

    `Code quality`/`Testing and debugging`::`How to read a traceback`: Divide correctly ⌛✅

--- Topics/How to read a traceback/Divide correctly/main.py
+++ Topics/How to read a traceback/Divide correctly/main.py
@@ -1,5 +1,8 @@
 def divide_by(number):
-    if number > 0:
-        print(100 / number)
-    else:
-        print(100 / (number / 2))
+    try:
+        if number > 0:
+            print(100 / number)
+        else:
+            print(100 / (number / 2))
+    except ZeroDivisionError:
+        print('Please enter a number that is not equal to zero')
