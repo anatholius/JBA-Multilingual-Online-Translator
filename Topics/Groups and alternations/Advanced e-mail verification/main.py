93e18a51cea9a397928b29fd4a70971899641c7a
Anatol Derbisz <anatholius@gmail.com>
2022-06-30 06:07:10 +0200

    `Exercises`/`Regular expressions`::`Groups and alternations`: What year is it today? ️⌛

---
+++ Topics/Groups and alternations/Advanced e-mail verification/main.py
@@ -0,0 +1,3 @@
+import re
+
+template = r'[a-z\d\-.=_]{6,30}@(gmail\.com|jetbrains\.org|hyperskill\.org)'
---
+++ Topics/Groups and alternations/What year is it today/main.py
@@ -0,0 +1,7 @@
+import re
+
+
+# put your regex in the variable template
+template = ""
+string = input()
+# compare the string and the template
