ac816b55c63caf719e13f303cc0ab381ef308a76
Anatol Derbisz <anatholius@gmail.com>
2022-06-30 22:59:48 +0200

    `Additional learning`/`Tuples`::`Operations with tuple`: Unpacking ⌛⌛

--- Topics/Operations with tuple/Unpacking/main.py
+++ Topics/Operations with tuple/Unpacking/main.py
@@ -1,14 +1,3 @@
 def unpack(input_tuple):
-    return (
-        item if isinstance(item, tuple) else item
-        for item in input_tuple
-     )
-    
-    u = []
-    for item in input_tuple:
-        if isinstance(item, tuple):
-            for i in item:
-                u.append(i)
-        else:
-            u.append(item)
-    return tuple(u)
+    # your code here
+    return unpacked
