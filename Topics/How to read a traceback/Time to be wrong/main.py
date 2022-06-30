b115a658696dc80ec097f71bc539a44f58297412
Anatol Derbisz <anatholius@gmail.com>
2022-06-30 06:56:38 +0200

    `Code quality`/`Testing and debugging`::`How to read a traceback`: Time to be wrong ⌛

---
+++ Topics/How to read a traceback/Time to be wrong/main.py
@@ -0,0 +1,8 @@
+def print_list(some_list):
+
+    while i < range(len(some_list)):
+        if some_list[i] % 2 == 0:
+            print(some_list[i])
+        if some_list[i] % 3 = 0:
+            print(some_list[i] % 3)
+        i += 1
